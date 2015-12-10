f780431a27de1d9832e97b50f942f15f151d4337
bnagallo <bryan.nagallo@gmail.com>
2015-12-10 18:38:24 -0500

    changed LOGF location

--- wrapper.sh
+++ wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## modify $LOGF directory
-LOGF="/home/bnagallo/Desktop/send_alert.log"
+LOGF="~/AppDynamics/Controller/custom/actions/postmsg/output/send_alert.log"
 . ./appd_alert.sh  "$@" >> $LOGF
 
 
